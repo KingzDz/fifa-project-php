8789666cad2c3ccf86b109f5d08fa89868e93ae2
Unknown <sybrandbos@gmail.com>
2019-04-18 10:59:11 +0200

    API map organized

--- api/read.php
+++ api/read.php
@@ -4,39 +4,44 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UFT-8");
 
 require 'config.php';
-include_once 'team.php';
+include_once 'recipe.php';
 
-$team = new Team($db);
+$recipe = new Recipe($db);
 
-$stmt = $team->Read();
+$stmt = $recipe->Read();
 $num = $stmt->rowCount();
 
 if($num > 0) {
 
-	$teamsArray=array();
-	$teamsArray["records"]=array();
+	$recipes_arr=array();
+	$recipes_arr["records"]=array();
 
 	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 		extract($row);
-		$team_item=array(
+		$recipe_item=array(
 			"id" => $id,
-			"teamname" => $teamname,
-			"players" => $players,
-			"leader" => $sybrand
+			"category_id" => $category_id,
+			"updated_at" => $updated_at,
+			"created_at" => $created_at,
+			"user_id" => $user_id,
+			"title" => $title,
+			"description" => $description,
+			"ingredients" => $ingredients,
+			"picture_url" => $picture_url
 		);
 
-		array_push($teamsArray["records"], $team_item);
+		array_push($recipes_arr["records"], $recipe_item);
 	}
 
 	http_response_code(200);
 
-	echo json_encode($teamsArray);
+	echo json_encode($recipes_arr);
 
 }
 else {
 	http_response_code(404);
 
 	echo json_encode(
-		array("message" => "No teams found.")
+		array("message" => "No recipes found.")
 	);
 }
