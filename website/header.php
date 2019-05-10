6a3b8759b42f627b146ff819c04186d9d72742ac
Evilman <timo02@me.com>
2019-05-10 09:31:36 +0200

    Quick bugfix

--- website/header.php
+++ website/header.php
@@ -1,13 +1,16 @@
 <?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 if($_SESSION['loggedin'] == true){
-    ?>
+?>
 <header>
     <div class="nav">
         <div class="navigation">
             <a href="index.php">Home</a>
             <img src="img/FIFA-logo.png" alt="FIFA-logo">
             <?php
-            if($_SESSION['admin'] == true){
+            if(!isset($_SESSION['admin'])){
                 ?>
                 <a href="admin-page.php">Account</a>
                 <?php
