75819a2093f07a16ff0c0c83b4c796d319bd9179
Evilman <timo02@me.com>
2019-05-17 09:29:20 +0200

    API fix
    de C# app werkte niet

--- website/API/read.php
+++ website/API/read.php
@@ -32,7 +32,46 @@ else if (isset($_GET['key']) && $_GET['key'] != 'J93hdb4Ua83AkVWo0cbxIsn2ibw3nlx
 }
 else if (isset($_GET['key']) && $_GET['key'] == 'J93hdb4Ua83AkVWo0cbxIsn2ibw3nlxX3') {
 
-	$_SESSION['api-key'] = 'J93hdb4Ua83AkVWo0cbxIsn2ibw3nlxX3';
-	header('Location: api.php');
+	//$_SESSION['api-key'] = 'J93hdb4Ua83AkVWo0cbxIsn2ibw3nlxX3';
+	//header('Location: api.php');
+	
+	// de C# app word niet doorgestuurd naar api.php
+	require '../config.php';
+	include_once 'team.php';
+
+	$team = new Team($db);
+
+	$stmt = $team->Read();
+	$num = $stmt->rowCount();
+
+	if($num > 0) {
+
+		$teamsArray=array();
+		$teamsArray["records"]=array();
+
+		while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+			extract($row);
+			$teamItem=array(
+				"id" => $id,
+				"teamname" => $teamname,
+				"players" => $players,
+				"leader" => $leader
+			);
+
+			array_push($teamsArray["records"], $teamItem);
+		}
+
+		http_response_code(200);
+
+		echo json_encode($teamsArray);
+
+	}
+	else {
+		http_response_code(404);
+
+		echo json_encode(
+			array("message" => "No teams found.")
+		);
+	}
 	exit;
 }
